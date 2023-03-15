6ec4a9687c85feb98aae3e405da02a481ea0d857
dev.ahmed.elsied <ahmed_elsayed@codebase-tech.com>
2023-03-15 23:28:30 +0200

    WIP

---
+++ resources/views/dashboard/child/bracelets/show.blade.php
@@ -0,0 +1,8 @@
+<x-ui::layout :title="__('Bracelet: :title',['title'=>$bracelet->child_name])" :breadcrumbs="[$bracelet->code,$bracelet->child_name]">
+    <x-slot:actions>
+    </x-slot:actions>
+    <x-slot:scripts>
+        {{-- @customvite('src/index.ts','vendor/bracelet/statistics') --}}
+    </x-slot:scripts>
+
+</x-ui::layout>
